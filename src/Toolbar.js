880e8ce5b79aa634375fb111c21432378d6a1ef0
Denis Akiyakov <newsiberian2015@yandex.ru>
2020-10-17 22:38:15 +0700

    refactor: Toolbar component to RFC feat: add `arrayMap` helper

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,70 +1,49 @@
+import * as React from 'react'
 import PropTypes from 'prop-types'
-import React from 'react'
 import clsx from 'clsx'
+
 import { navigate } from './utils/constants'
+import { arrayMap } from './utils/helpers'
 
-class Toolbar extends React.Component {
-  render() {
-    let {
-      localizer: { messages },
-      label,
-    } = this.props
-
-    return (
-      <div className="rbc-toolbar">
-        <span className="rbc-btn-group">
-          <button
-            type="button"
-            onClick={this.navigate.bind(null, navigate.TODAY)}
-          >
-            {messages.today}
-          </button>
-          <button
-            type="button"
-            onClick={this.navigate.bind(null, navigate.PREVIOUS)}
-          >
-            {messages.previous}
-          </button>
-          <button
-            type="button"
-            onClick={this.navigate.bind(null, navigate.NEXT)}
-          >
-            {messages.next}
-          </button>
-        </span>
-
-        <span className="rbc-toolbar-label">{label}</span>
-
-        <span className="rbc-btn-group">{this.viewNamesGroup(messages)}</span>
-      </div>
-    )
+function Toolbar({ label, localizer, view, views, onNavigate, onView }) {
+  function handleNavigate(action) {
+    onNavigate(action)
   }
 
-  navigate = action => {
-    this.props.onNavigate(action)
+  function handleView(view) {
+    onView(view)
   }
 
-  view = view => {
-    this.props.onView(view)
-  }
-
-  viewNamesGroup(messages) {
-    let viewNames = this.props.views
-    const view = this.props.view
-
-    if (viewNames.length > 1) {
-      return viewNames.map(name => (
-        <button
-          type="button"
-          key={name}
-          className={clsx({ 'rbc-active': view === name })}
-          onClick={this.view.bind(null, name)}
-        >
-          {messages[name]}
+  return (
+    <div className="rbc-toolbar">
+      <span className="rbc-btn-group">
+        <button type="button" onClick={() => handleNavigate(navigate.TODAY)}>
+          {localizer.messages.today}
         </button>
-      ))
-    }
-  }
+        <button type="button" onClick={() => handleNavigate(navigate.PREVIOUS)}>
+          {localizer.messages.previous}
+        </button>
+        <button type="button" onClick={() => handleNavigate(navigate.NEXT)}>
+          {localizer.messages.next}
+        </button>
+      </span>
+
+      <span className="rbc-toolbar-label">{label}</span>
+
+      <span className="rbc-btn-group">
+        {arrayMap(views)((name) => (
+          <button
+            key={name}
+            type="button"
+            className={clsx({ 'rbc-active': view === name })}
+            onClick={() => handleView(name)}
+          >
+            {localizer.messages[name]}
+          </button>
+        ))}
+      </span>
+    </div>
+  )
 }
 
 Toolbar.propTypes = {
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -19,3 +19,37 @@ export function isFirstFocusedRender(component) {
     (component.state.focused && (component._firstFocus = true))
   )
 }
+
+/**
+ * Determines is an object is a filled array or not and execute arr.map with
+ * given callback
+ *
+ * Can be used in React components like instead of such cases:
+ * return (
+ *   <Select>
+ *     {Boolean(Array.isArray(arr) && arr.length) && arr.map(a => (
+ *       <MenuItem key={a.id} value={a.id}>
+ *         {a.name}
+ *       </MenuItem>
+ *     ))}
+ *   </Select>
+ * );
+ *
+ * to this:
+ *
+ * return (
+ *   <Select>
+ *     {arrayMap(arr)(a => (
+ *       <MenuItem key={a.id} value={a.id}>
+ *         {a.name}
+ *       </MenuItem>
+ *     ))}
+ *   </Select>
+ * );
+ *
+ * @param {Array} arr - possible array
+ * @function {function} callback - map condition
+ * @return {function(function): Array | null}
+ */
+export const arrayMap = arr => callback =>
+  Array.isArray(arr) && arr.length ? arr.map(callback) : null;
