a2ebed27aef67e8835ed136427a95f3041936e59
Denis Akiyakov <newsiberian2015@yandex.ru>
2020-10-22 13:18:16 +0700

    refactor: `Popup` component moved to RFC

--- src/Popup.js
+++ src/Popup.js
@@ -8,87 +8,95 @@ import * as dates from './utils/dates'
 import EventCell from './EventCell'
 import { isSelected } from './utils/selection'
 
-class Popup extends React.Component {
-  componentDidMount() {
-    let { popupOffset = 5, popperRef } = this.props,
-      { top, left, width, height } = getOffset(popperRef.current),
-      viewBottom = window.innerHeight + getScrollTop(window),
-      viewRight = window.innerWidth + getScrollLeft(window),
-      bottom = top + height,
-      right = left + width
+/**
+ * The Overlay component, of react-overlays, creates a ref that is passed to the Popup, and
+ * requires proper ref forwarding to be used without error
+ */
+const Popup = React.forwardRef(function Popup(
+  {
+    position,
+    popupOffset,
+    events,
+    selected,
+
+    accessors,
+    components,
+    getters,
+    localizer,
+    onSelect,
+    onDoubleClick,
+    onKeyPress,
+    handleDragStart,
+    show,
+    slotStart,
+    slotEnd,
+    style,
+  },
+  ref
+) {
+  const [offset, setOffset] = React.useState({ topOffset: 0, leftOffset: 0 })
+
+  React.useEffect(() => {
+    const { top, left, width, height } = getOffset(ref.current)
+    const viewBottom = window.innerHeight + getScrollTop(window)
+    const viewRight = window.innerWidth + getScrollLeft(window)
+    const bottom = top + height
+    const right = left + width
 
     if (bottom > viewBottom || right > viewRight) {
-      let topOffset, leftOffset
+      let topOffset = 0
+      let leftOffset = 0
 
-      if (bottom > viewBottom)
+      if (bottom > viewBottom) {
         topOffset = bottom - viewBottom + (popupOffset.y || +popupOffset || 0)
-      if (right > viewRight)
+      }
+      if (right > viewRight) {
         leftOffset = right - viewRight + (popupOffset.x || +popupOffset || 0)
+      }
 
-      this.setState({ topOffset, leftOffset }) //eslint-disable-line
+      setOffset({ topOffset, leftOffset })
     }
+  }, [])
+
+  const computedStyle = {
+    top: -offset.topOffset,
+    left: -offset.leftOffset,
+    minWidth: position.width + position.width / 2,
   }
 
-  render() {
-    let {
-      events,
-      selected,
-      getters,
-      accessors,
-      components,
-      onSelect,
-      onDoubleClick,
-      onKeyPress,
-      slotStart,
-      slotEnd,
-      localizer,
-      popperRef,
-    } = this.props
-
-    let { width } = this.props.position,
-      topOffset = (this.state || {}).topOffset || 0,
-      leftOffset = (this.state || {}).leftOffset || 0
-
-    let style = {
-      top: -topOffset,
-      left: -leftOffset,
-      minWidth: width + width / 2,
-    }
-
-    return (
-      <div
-        style={{ ...this.props.style, ...style }}
-        className="rbc-overlay"
-        ref={popperRef}
-      >
-        <div className="rbc-overlay-header">
-          {localizer.format(slotStart, 'dayHeaderFormat')}
-        </div>
-        {events.map((event, idx) => (
-          <EventCell
-            key={idx}
-            type="popup"
-            event={event}
-            getters={getters}
-            onSelect={onSelect}
-            accessors={accessors}
-            components={components}
-            onDoubleClick={onDoubleClick}
-            onKeyPress={onKeyPress}
-            continuesPrior={dates.lt(accessors.end(event), slotStart, 'day')}
-            continuesAfter={dates.gte(accessors.start(event), slotEnd, 'day')}
-            slotStart={slotStart}
-            slotEnd={slotEnd}
-            selected={isSelected(event, selected)}
-            draggable={true}
-            onDragStart={() => this.props.handleDragStart(event)}
-            onDragEnd={() => this.props.show()}
-          />
-        ))}
+  return (
+    <div
+      style={{ ...style, ...computedStyle }}
+      className="rbc-overlay"
+      ref={ref}
+    >
+      <div className="rbc-overlay-header">
+        {localizer.format(slotStart, 'dayHeaderFormat')}
       </div>
-    )
-  }
-}
+      {events.map((event, idx) => (
+        <EventCell
+          draggable
+          key={idx}
+          type="popup"
+          event={event}
+          getters={getters}
+          onSelect={onSelect}
+          accessors={accessors}
+          components={components}
+          onDoubleClick={onDoubleClick}
+          onKeyPress={onKeyPress}
+          continuesPrior={dates.lt(accessors.end(event), slotStart, 'day')}
+          continuesAfter={dates.gte(accessors.start(event), slotEnd, 'day')}
+          slotStart={slotStart}
+          slotEnd={slotEnd}
+          selected={isSelected(event, selected)}
+          onDragStart={() => handleDragStart(event)}
+          onDragEnd={show}
+        />
+      ))}
+    </div>
+  )
+})
 
 Popup.propTypes = {
   position: PropTypes.object,
@@ -113,16 +121,7 @@ Popup.propTypes = {
   show: PropTypes.func,
   slotStart: PropTypes.instanceOf(Date),
   slotEnd: PropTypes.number,
-  popperRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.Element }),
-  ]),
+  style: PropTypes.object,
 }
 
-/**
- * The Overlay component, of react-overlays, creates a ref that is passed to the Popup, and
- * requires proper ref forwarding to be used without error
- */
-export default React.forwardRef((props, ref) => (
-  <Popup popperRef={ref} {...props} />
-))
+export default Popup
