e803de9bae277b7abe4f987e56fddaa1ffaa30ee
Denis Akiyakov <newsiberian2015@yandex.ru>
2020-10-25 14:05:37 +0700

    chore: Storybook cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,3 @@
-// import { configure, addParameters } from '@storybook/react'
-// import rbcTheme from './rbc.theme'
-//
-// addParameters({
-//   options: {
-//     theme: rbcTheme,
-//   },
-// })
-//
-// function loadStories() {
-//   require('../stories')
-// }
-//
-// configure(loadStories, module)
 module.exports = {
   stories: ['../stories/*.stories.@(js|mdx)'],
   addons: ['@storybook/addon-actions', '@storybook/addon-controls'],
